4c7e60bda02a18cb2e8a9f94066051408699b2b4
Anil <themacharamguy@gmail.com>
2025-02-16 21:28:41 +0530

    journal entries with request mapping

---
+++ journalEntry/src/main/java/com/mernvids/sb/journalEntry/controller/JournalEntryController.java
@@ -0,0 +1,43 @@
+package com.mernvids.sb.journalEntry.controller;
+
+import com.mernvids.sb.journalEntry.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.*;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries=new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll()
+    {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myID}")
+    public JournalEntry getByID(@PathVariable Long myID){
+        return journalEntries.get(myID);
+    }
+
+    @DeleteMapping("id/{myID}")
+    public JournalEntry deleteByID(@PathVariable Long myID){
+        return journalEntries.remove(myID);
+    }
+
+    @PutMapping("id/{id}")
+    public JournalEntry updateByID(@PathVariable Long id, @RequestBody JournalEntry myEntry)
+    {
+        return journalEntries.put(id,myEntry);
+    }
+    }
+
---
+++ journalEntry/src/main/java/com/mernvids/sb/journalEntry/entity/JournalEntry.java
@@ -0,0 +1,32 @@
+package com.mernvids.sb.journalEntry.entity;
+
+public class JournalEntry {
+    private long id;
+    private String name;
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+}
